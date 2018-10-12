9dd8814d5405fa3605d11d41a720c94155c69517
agentle <latiosa811@charter.net>
2018-10-12 14:55:26 -0400

    Added Ackley Function

--- TestFunctions.java
+++ TestFunctions.java
@@ -3,6 +3,26 @@ package PSO_Neighborhoods;
 
 public class TestFunctions {
 	public static void main(String[] args) {
+
 	}
+
+	public static double Ackley(double[] values) {
+
+		double a = 20.0;
+		double b = 0.2;
+		double c = 2.0 * Math.PI;
+
+		double firstSum = 0;
+		double secondSum = 0;
+		for (int i = 0; i < values.length; i++) {
+			firstSum += values[i] * values[i];
+			secondSum += Math.cos(c * values[i]);
+		}
+
+		double total = -a * Math.exp(-b * Math.sqrt(firstSum/values.length)) - Math.exp(secondSum/values.length) + a + Math.E;
+		return total;
+
+	}
+
 }
 
