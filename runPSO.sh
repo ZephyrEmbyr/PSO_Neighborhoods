894face5194504d8c64fa4850677567356b25e73
agentle <latiosa811@charter.net>
2018-10-14 22:56:16 -0400

    added a shell script to run the program

--- runPSO.sh
+++ runPSO.sh
@@ -2,41 +2,33 @@
 
 echo "Running PSO Code"
 
-# filetest="maxsat-problems/maxsat-crafted/MAXCUT/SPINGLASS/t3pm3-5555.spn.cnf"
+javac PSO.java 
 
-top[0]="gl"
-top[1]="ri"
-top[2]="vn"
-top[3]="ra"
+topology[0]="gl"
+topology[1]="ri"
+topology[2]="vn"
+topology[3]="ra"
 
-size[0]=16
-size[1]=30
-size[2]=49
+numParticles[0]=16
+numParticles[1]=30
+numParticles[2]=49
 
 iters=10000
 
-func[0]="rok"
-func[1]="ack"
-func[2]="ras"
+function[0]="rok"
+function[1]="ack"
+function[2]="ras"
 
 dim=30
 
-for topology in $(seq )
-numind=100
-selectiontype="ts"
-crossovermethod="1c"
-crossoverprob=0.7
-mutationprob=0.01
-numgen=1000
-whichalgo="g"
-
-crossover[0]="1c"
-crossover[1]="uc"
-
-for method in $(seq 0 1 2)
+for top in $(seq 0 1 3)
 do
-    # echo $probability
-    # echo ${mutAmt[probability]}
-    printf "Crossover Method: %s\n" "${crossover[method]}" >> "allOutput/gaOutput/runCrossoverMethod.txt"
-    ./evolAlg $filetest $numind $selectiontype ${crossover[method]} $crossoverprob $mutationprob $numgen $whichalgo >> "allOutput/gaOutput/runCrossoverMethod.txt"
+	for size in $(seq 0 1 2)
+	do
+		for func in $(seq 0 1 2)
+		do
+			java PSO ${topology[top]} ${numParticles[size]} $iters ${function[func]} $dim
+		done
+	done
 done
+
